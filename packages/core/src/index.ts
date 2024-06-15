47f7383830bce6da4c21b1b3934177d06f5deaab
eric.crowell <eric.crowell@ecrow.dev>
2024-06-15 20:24:19 +0200

    dev: Added clmg to the public api

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,9 +1,11 @@
 import { nop } from './utility/nop';
 import { memoize } from './utility/memoize';
 import { clsx } from './utility/clsx';
+import { clmg } from './utility/clmg';
 
 export {
   clsx,
+  clmg,
   memoize,
   nop
 };
