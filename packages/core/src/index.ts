9e0f84071e7bed46831ba72d48517d7035f0169a
eric.crowell <eric.crowell@ecrow.dev>
2024-06-16 12:20:58 +0200

    feat: Publicly exposed nanoid api

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,5 @@
 import { nop } from './utility/nop';
+import { nanoid } from './utility/nanoid';
 import { memoize } from './utility/memoize';
 import { clsx } from './utility/clsx';
 import { clmg } from './utility/clmg';
@@ -7,5 +8,6 @@ export {
   clsx,
   clmg,
   memoize,
-  nop
+  nop,
+  nanoid
 };
